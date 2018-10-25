ae0b6cde6817db1b6301930d884505340946c62a
wzhao <wzhao@alice.andrew.cmu.edu>
2018-10-25 05:14:56 +0000

    feat:Process data

---
+++ src/utils/plot_data.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+import os
+import numpy as np
+import matplotlib
+from matplotlib import pyplot as plt
+from scipy.io import wavfile as siowav
+import pdb
+
+
+data_root = "../../data/free-spoken-digit-dataset/recordings/"
+
+test_file = "0_jackson_5.wav"
+
+sample_rate, samples = siowav.read(os.path.join(data_root, test_file))
+print('sample_rate: ', sample_rate)
+print('signal length: ', len(samples), ' ', len(samples) / sample_rate, 's')
+
+fig = plt.figure(figsize=(8, 8))
+
+ax1 = fig.add_subplot(211)
+ax1.plot(range(len(samples)), samples, 'r', alpha=0.5)
+ax1.axis('tight')
+ax1.grid(True)
+ax1.set_xlabel('t')
+ax1.set_ylabel('amp')
+ax1.set_title('jackson 5, Fs={}'.format(sample_rate))
+
+# cmap = 'viridis'
+cmap = 'jet'
+
+ax2 = fig.add_subplot(223)
+spec, freqs, ts, im = ax2.specgram(
+    samples,
+    Fs=sample_rate,
+    NFFT=256,
+    noverlap=128,
+    mode="magnitude",
+    scale="dB",
+    cmap=cmap
+    )
+ax2.axis('off')
+ax2.set_title('NFFT={} noverlap={}'.format(256, 128))
+print('spectrogram shape: ', spec.shape)
+
+ax3 = fig.add_subplot(224)
+spec, freqs, ts, im = ax3.specgram(
+    samples,
+    Fs=sample_rate,
+    NFFT=454,
+    noverlap=421,
+    mode="magnitude",
+    scale="dB",
+    cmap=cmap
+    )
+ax3.axis('off')
+ax3.set_title('NFFT={} noverlap={}'.format(454, 421))
+print('spectrogram shape: ', spec.shape)
+
+fig.tight_layout()
+# fig.show()
+fig.savefig('sample_plot_jackson_5.jpg')
+
+plt.figure(figsize=(8, 8))
+plt.imshow(im.get_array(), cmap=cmap)
+# plt.show()
+plt.imsave('sample_spec_jackson_5.jpg',
+           im.get_array(), cmap=cmap)
---
+++ src/utils/process_data.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import numpy as np
+from scipy.io import wavfile as siowav
+from scipy.signal import get_window, spectrogram
+import pdb
+
+
+def pad_signal(sig, pad_length=8000, seed=1234):
+    '''
+    Pad signal to fixed length in random position.
+
+    Parameters
+    ----------
+    sig: numpy.array
+    pad_length: int
+
+    Returns
+    -------
+    sig_pad: numpy.array
+    '''
+    np.random.seed(seed)
+
+    sig_len = len(sig)
+
+    if sig_len == pad_length:
+        return sig
+
+    if sig_len > pad_length:  # extract middle
+        mid = sig_len // 2
+        sp = int(mid - pad_length / 2)
+        ep = int(mid + pad_length / 2)
+        return sig[sp:ep]
+
+    if sig_len < pad_length:  # random pad
+        len_diff = pad_length - sig_len
+        split = np.random.randint(0, len_diff + 1)  # random split
+        pad_s = int(split)
+        pad_e = int(len_diff - split)
+        sig_pad = np.pad(sig, (pad_s, pad_e), 'constant', constant_values=0)
+        return sig_pad
+
+
+def normalize_signal(sig, percentile=95.):
+    '''
+    Normalize signal against the pth percentile.
+    '''
+    return sig / np.percentile(sig, percentile)
+
+
+data_root = "../../data/free-spoken-digit-dataset/recordings/"
+data_list = './recording.list'
+save_dir = "../../data/free-spoken-digit-dataset/processed/spectrograms"
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
+files = [l.rstrip('\n') for l in open(data_list)]
+
+win = get_window('hann', 454)
+
+for f in files:
+    # Read signal
+    sample_rate, samples = siowav.read(os.path.join(data_root, f))
+    assert sample_rate == 8000, "{}: incompatible sample rate"\
+        " need 8000 but got {}".format(f, sample_rate)
+
+    # Pad
+    samples.astype('float32')
+    samples = pad_signal(samples)
+    assert len(samples) == 8000, "{}: incorrect signal length"\
+        " need 8000 but got {}".format(f, len(samples))
+
+    # Normalize
+    # samples = normalize_signal(samples)
+
+    # Compute spectrogram
+    _, _, spec = spectrogram(
+        samples,
+        fs=sample_rate,
+        window=win,
+        nperseg=454,
+        noverlap=421,
+        nfft=454,
+        detrend='constant',
+        return_onesided=True,
+        scaling='density',
+        mode='magnitude')
+
+    # Flip and drop
+    spec = np.flipud(spec)
+    spec = spec[1:, :-2]
+    assert spec.shape == (227, 227), "{}: incorrect spectrogram shape"\
+        " need (227, 227) but got {}".format(f, spec.shape)
+
+    # Save
+    save_f = os.path.join(save_dir, os.path.splitext(f)[0])
+    np.save(save_f, spec)
+    print("saved {}.npy".format(save_f))
